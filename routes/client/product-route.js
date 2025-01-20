95242bc516d185677bbe65b290463cf0170a0736
NgKien94 <quangkienofqn@gmail.com>
2025-01-20 20:18:03 +0700

    Initial commit

---
+++ routes/client/product-route.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const controller = require('../../controllers/client/product-controller')
+const router = express.Router()
+
+router.get('/', controller.index)
+
+
+// router.get('/products/create', (req,res)=>{
+//     res.render('client/pages/products/index.pug')
+// })
+
+
+// router.get('/products/edit', (req,res)=>{
+//     res.render('client/pages/products/index.pug')
+// })
+
+module.exports = router;
