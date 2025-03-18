61bdeb8ed29532ddc6e21fdb3269f624a8a7263f
NgKien94 <quangkienofqn@gmail.com>
2025-03-18 22:21:07 +0700

    Authorization

--- middlewares/admin/auth-middleware.js
+++ middlewares/admin/auth-middleware.js
@@ -1,5 +1,6 @@
 const systemConfig = require('../../config/system')
 const Account = require('../../models/account-model')
+const Role = require('../../models/role-model')
 
 module.exports.requireAuth = async (req, res, next) => {
     if (!req.cookies.token) {
@@ -7,8 +8,15 @@ module.exports.requireAuth = async (req, res, next) => {
         return;
     }   
     else {
-        const user = await Account.findOne({token: req.cookies.token})
+        const user = await Account.findOne({token: req.cookies.token}).select("-password")
         if(user){
+            const role = await Role.findOne({
+                _id: user.role_id
+            }).select("title permissions")
+
+           
+            res.locals.user = user; // trả user hiện tại ra cho view, để authorization
+             res.locals.role = role; // trả role hiện tại ra cho view, để authorization
             next();
         }
         else{
