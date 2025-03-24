d245f03c7f5954402bebc13aad240d71b7538913
NgKien94 <quangkienofqn@gmail.com>
2025-03-25 06:02:41 +0700

    Detail of product page client

--- controllers/client/product-controller.js
+++ controllers/client/product-controller.js
@@ -22,16 +22,26 @@ module.exports.index = async (req, res) => {
     })
 }
 
+// [GET] get detail product /detail/:slugProduct
 module.exports.detail = async (req, res) => {
     try {
-        console.log(req.params.slug)
+  
         const find = {
             deleted: false,
-            slug: req.params.slug,
+            slug: req.params.slugProduct,
             status: "active"
         }
         const product = await Product.findOne(find)
-        console.log(product)
+        if(product.product_category_id){
+            const category = await ProductCategory.findOne({
+                _id: product.product_category_id,
+                status: "active",
+                deleted: false
+            })
+
+            product.category = category;
+        }
+        product.priceNew = productsHelper.priceNewProduct(product);
 
         res.render("client/pages/products/detail", {
             pageTitle: product.title,
