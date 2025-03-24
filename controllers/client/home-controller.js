8d6b9be57e8f966867cedb160047f43e03853ea2
NgKien94 <quangkienofqn@gmail.com>
2025-03-25 03:32:56 +0700

    Show latest products for Client

--- controllers/client/home-controller.js
+++ controllers/client/home-controller.js
@@ -10,10 +10,20 @@ module.exports.index = async (req,res) =>{
         status: "active"
     }).limit(6);
     //End lấy ra sản phẩm nổi bật
-    const newProducts =  productsHelper.priceNewProducts(productsFeatured);
+    const newProductsFeatured =  productsHelper.priceNewProducts(productsFeatured);
     
+    //Lấy ra sản phẩm mới nhất
+    const productsNew = await Product.find({
+        deleted: false,
+        status: "active"
+    }).sort({position: "desc"}).limit(6)
+
+    const newProductsNew = productsHelper.priceNewProducts(productsNew)
+    //End get latest products
+
     res.render('client/pages/home/index.pug', {
         pageTitle: 'Home page',
-        productsFeatured: newProducts
+        productsFeatured: newProductsFeatured,
+        productsNew: newProductsNew
     });
 }
