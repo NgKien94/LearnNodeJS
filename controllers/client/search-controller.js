68ed7576a9eb1da7d8572090f41940a821398403
NgKien94 <quangkienofqn@gmail.com>
2025-03-25 20:03:47 +0700

    Search products for clients

---
+++ controllers/client/search-controller.js
@@ -0,0 +1,25 @@
+const Product = require('../../models/product-model')
+const productsHelper = require('../../helpers/products')
+
+module.exports.index = async (req,res) =>{
+    const keyword = req.query.keyword;
+    let newProducts = []
+    if(keyword){
+        const keywordRegex = new RegExp(keyword,"i")
+
+        const products = await Product.find({
+            title: keywordRegex,
+            deleted: false,
+            status: "active"
+        })
+
+        newProducts = productsHelper.priceNewProducts(products)
+        console.log(products)
+    }
+
+    res.render('client/pages/search/index',{
+        pageTitle: 'Kết quả tìm kiếm',
+        keyword: keyword,
+        products: newProducts
+    })
+}
