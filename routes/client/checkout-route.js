74a13b00625fcc85562e5c6d429c5c169bc90b15
NgKien94 <quangkienofqn@gmail.com>
2025-04-05 23:00:25 +0700

    Checkout page

---
+++ routes/client/checkout-route.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const controller = require("../../controllers/client/checkout-controller");
+
+// const authMiddleware = require("../../middlewares/client/auth.middleware");
+
+router.get("/", controller.index);
+
+
+module.exports = router;
