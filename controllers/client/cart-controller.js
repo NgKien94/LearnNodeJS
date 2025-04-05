55b1e5c6635133207eedbf003cf73e6b6392d688
NgKien94 <quangkienofqn@gmail.com>
2025-04-05 20:21:16 +0700

    Add mini cart in header

--- controllers/client/cart-controller.js
+++ controllers/client/cart-controller.js
@@ -3,44 +3,50 @@ const Cart = require('../../models/cart-model')
 
 //[POST] /cart/add/:productId
 module.exports.addPost = async (req, res) => {
-
     const cartId = req.cookies.cartId;
-    const productId = req.params.productId
-    const quantity = parseInt(req.body.quantity)
+    const productId = req.params.productId;
+    const quantity = parseInt(req.body.quantity);
 
-    const cart = await Cart.findOne({
-        _id: cartId
-    });
+    let cart = await Cart.findOne({ _id: cartId });
 
+    // Nếu cart không tồn tại, tạo mới
+    if (!cart) {
+        cart = await Cart.create({
+            _id: cartId,
+            products: [{
+                product_id: productId,
+                quantity: quantity
+            }]
+        });
 
+        req.flash('success','Tạo giỏ hàng và thêm sản phẩm thành công');
+        return res.redirect(req.get('Referrer') || '/products');
+    }
+
+    // Nếu cart đã tồn tại
     const existProductInCart = cart.products.find(item => item.product_id == productId);
     if (existProductInCart) {
         const newQuantity = existProductInCart.quantity + quantity;
-        // Cập nhật quantity của product tương ứng
         await Cart.updateOne(
             {
-            _id: cartId,
-            'products.product_id': productId
-        },{
-            'products.$.quantity': newQuantity
-        }
-    )
-    }
-    else {
+                _id: cartId,
+                'products.product_id': productId
+            },
+            {
+                'products.$.quantity': newQuantity
+            }
+        );
+    } else {
         const objectCart = {
             product_id: productId,
             quantity: quantity
-        }
-
-
-        await Cart.updateOne({
-            _id: cartId
-        }, {
-            $push: { products: objectCart }
-        })
+        };
+        await Cart.updateOne(
+            { _id: cartId },
+            { $push: { products: objectCart } }
+        );
     }
 
-
-    req.flash('success','Thêm sản phẩm vào giỏ hàng thành công')
-    res.redirect(req.get('Referrer') || '/products')
-}
+    req.flash('success','Thêm sản phẩm vào giỏ hàng thành công');
+    res.redirect(req.get('Referrer') || '/products');
+};
--- middlewares/client/cart-middleware.js
+++ middlewares/client/cart-middleware.js
@@ -13,6 +13,14 @@ module.exports.cartId =  async (req,res,next) =>{
     }
     else{
         // Đã có giỏ hàng
+        const cart = await Cart.findOne({
+            _id: req.cookies.cartId
+        });
+       
+        cart.totalQuantity = cart.products.reduce((sum,item) =>sum+item.quantity,0)
+        
+        res.locals.miniCart = cart;
+
     }
     next();
 }
