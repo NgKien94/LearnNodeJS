c6f5c43ba889b0354cdab979f8f1caa57b0ab4a0
NgKien94 <quangkienofqn@gmail.com>
2025-04-05 21:54:41 +0700

    Delete products of cart

--- controllers/client/cart-controller.js
+++ controllers/client/cart-controller.js
@@ -10,22 +10,22 @@ module.exports.index = async (req, res) => {
     const cart = await Cart.findOne({
         _id: cartId
     })
-    
 
-    if(cart.products.length > 0){
-        for(const item of cart.products){
+
+    if (cart.products.length > 0) {
+        for (const item of cart.products) {
             const productId = item.product_id;
             const productInfo = await Product.findOne({
                 _id: productId
             });
-            
+
             productInfo.priceNew = productHelper.priceNewProduct(productInfo);
             item.productInfo = productInfo;
-            item.totalPrice = item.quantity*productInfo.priceNew;
+            item.totalPrice = item.quantity * productInfo.priceNew;
         }
     }
 
-    cart.totalPrice = cart.products.reduce((sum,item) => sum +item.totalPrice,0);
+    cart.totalPrice = cart.products.reduce((sum, item) => sum + item.totalPrice, 0);
 
     res.render('client/pages/cart/index.pug', {
         pageTitle: 'Giỏ hàng',
@@ -51,7 +51,7 @@ module.exports.addPost = async (req, res) => {
             }]
         });
 
-        req.flash('success','Tạo giỏ hàng và thêm sản phẩm thành công');
+        req.flash('success', 'Tạo giỏ hàng và thêm sản phẩm thành công');
         return res.redirect(req.get('Referrer') || '/products');
     }
 
@@ -79,8 +79,27 @@ module.exports.addPost = async (req, res) => {
         );
     }
 
-    req.flash('success','Thêm sản phẩm vào giỏ hàng thành công');
+    req.flash('success', 'Thêm sản phẩm vào giỏ hàng thành công');
     res.redirect(req.get('Referrer') || '/products');
 };
 
+//[DELETE] /cart/delete/:productId
+module.exports.delete = async (req, res) => {
+    const cartId = req.cookies.cartId;
 
+    const productId = req.params.productId;
+    console.log(productId);
+    await Cart.updateOne(
+        {
+            _id: cartId,
+        }, {
+        "$pull": {
+            "products": {
+                "product_id": productId
+            }
+        }
+    })
+
+    req.flash('success', 'Xóa sản phẩm khỏi giỏ hàng thành công');
+    res.redirect(req.get('Referrer') || '/products');
+}
