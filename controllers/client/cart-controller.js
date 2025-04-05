36ba5f436bc16dda8619c30064b7fd333f972394
NgKien94 <quangkienofqn@gmail.com>
2025-04-05 21:29:08 +0700

    Cart product page

--- controllers/client/cart-controller.js
+++ controllers/client/cart-controller.js
@@ -1,5 +1,37 @@
 const Cart = require('../../models/cart-model')
+const Product = require('../../models/product-model')
+const productHelper = require('../../helpers/products')
 
+//[GET] /cart
+module.exports.index = async (req, res) => {
+
+    const cartId = req.cookies.cartId;
+
+    const cart = await Cart.findOne({
+        _id: cartId
+    })
+    
+
+    if(cart.products.length > 0){
+        for(const item of cart.products){
+            const productId = item.product_id;
+            const productInfo = await Product.findOne({
+                _id: productId
+            });
+            
+            productInfo.priceNew = productHelper.priceNewProduct(productInfo);
+            item.productInfo = productInfo;
+            item.totalPrice = item.quantity*productInfo.priceNew;
+        }
+    }
+
+    cart.totalPrice = cart.products.reduce((sum,item) => sum +item.totalPrice,0);
+
+    res.render('client/pages/cart/index.pug', {
+        pageTitle: 'Giỏ hàng',
+        cartDetail: cart
+    });
+}
 
 //[POST] /cart/add/:productId
 module.exports.addPost = async (req, res) => {
@@ -50,3 +82,5 @@ module.exports.addPost = async (req, res) => {
     req.flash('success','Thêm sản phẩm vào giỏ hàng thành công');
     res.redirect(req.get('Referrer') || '/products');
 };
+
+
--- routes/client/cart-route.js
+++ routes/client/cart-route.js
@@ -3,6 +3,7 @@ const controller = require('../../controllers/client/cart-controller')
 
 const router = express.Router()
 
+router.get('/',controller.index)
 router.post('/add/:productId', controller.addPost)
 
 
