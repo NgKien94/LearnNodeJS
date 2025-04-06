b5aebf1228184750f35b575dd2a2e2d24a45316f
NgKien94 <quangkienofqn@gmail.com>
2025-04-06 16:22:19 +0700

    Register page

---
+++ validates/client/user-validate.js
@@ -0,0 +1,33 @@
+module.exports.registerPost = (req, res, next) => {
+	if(!req.body.fullName){
+		req.flash("error", `Họ tên không được bỏ trống`);
+		res.redirect("back");
+		return;
+	}
+  if(!req.body.email){
+		req.flash("error", `Email không được bỏ trống`);
+		res.redirect("back");
+		return;
+	}
+  if(!req.body.password){
+		req.flash("error", `Mật khẩu không được bỏ trống`);
+		res.redirect("back");
+		return;
+	}
+	next();
+};
+
+module.exports.loginPost = (req, res, next) => {
+  if(!req.body.email){
+		req.flash("error", `Email không được bỏ trống`);
+		res.redirect("back");
+		return;
+	}
+  if(!req.body.password){
+		req.flash("error", `Mật khẩu không được bỏ trống`);
+		res.redirect("back");
+		return;
+	}
+	next();
+};
+
