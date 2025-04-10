b7f461a0bbba180bc686521fa47608796d484234
NgKien94 <quangkienofqn@gmail.com>
2025-04-10 22:53:20 +0700

    Basic chat

--- controllers/client/chat-controller.js
+++ controllers/client/chat-controller.js
@@ -1,12 +1,42 @@
+const Chat = require('../../models/chat-model')
+const User = require('../../models/user-model')
+
 //[GET] /chat/
-module.exports.index = (req, res) => {
+module.exports.index =  async (req, res) => {
+
+    const userId = res.locals.user.id 
+
     //Socket io
-    _io.on('connection',(socket) =>{
-        console.log('A user connected ',socket.id)
+    _io.once('connection',(socket) =>{
+        socket.on("CLIENT_SEND_MESSAGE", async (content)=>{
+
+            const chat = new Chat({
+                user_id: userId,
+                content: content
+            });
+
+            await chat.save()
+        })
       })
+
+      // Dùng once để kết nối một lần duy nhất, không bị reload id sau mỗi lần load trang
     //End Socket io
+    
+    // Lấy ra data
+    const chats = await Chat.find({
+        deleted: false
+    })
+
+    for( const chat of  chats){
+        const infoUser = await User.findOne({
+            _id: chat.user_id
+        }).select("fullName");
+        chat.infoUser = infoUser
+    }
+    console.log(chats)
 
     res.render('client/pages/chat/index', {
-        pageTitle: 'Chat'
+        pageTitle: 'Chat',
+        chats: chats
     })
 }
